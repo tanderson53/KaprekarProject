db360c30eedae0ea15704b8ebd0beecc19e295ec
tanderson53 <42656935+tanderson53@users.noreply.github.com>
2018-09-07 23:31:44 -0500

    error2
    Moved declaration int n to beginning of the code

--- c_version/kaprekarDemo.c
+++ c_version/kaprekarDemo.c
@@ -4,13 +4,11 @@
 #include "kaprekarUtils.h"
 
 int main(int argc, char **argv) {
-
+int n = atoi(argv[1]);
   if(argc [1] != 2) {
     fprintf(stderr, "Usage: %s n\n", argv[0]);
     exit(1);
   }
-
-  int n = atoi(argv[1]);
 
   if(isKaprekar(n)) {
     printf("%d is a Kaprekar Number!\n", n);
